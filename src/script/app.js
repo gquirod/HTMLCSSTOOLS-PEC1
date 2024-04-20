8e9faebe830af946e9d43b7c41cdb5c4ae1fc6d3
guiquirod <guillequirod@gmail.com>
2024-04-21 01:55:05 +0200

    trying different approach

--- src/script/app.js
+++ src/script/app.js
@@ -2,18 +2,32 @@ var footer = document.getElementById('footerMenu');
 var menuButton = document.getElementById('menuButton');
 import VanillaTilt from 'vanilla-tilt';
 
-if (window.innerWidth > 768){
-  footer.addEventListener('mouseover', function () {
-    menuButton.classList.add('hovered');
-  });
-  
-  
-  footer.addEventListener('mouseout', function () {
-    menuButton.classList.remove('hovered');
-  });
+function removeHoveredState(){
+  menuButton.classList.remove('hovered');
+  footer.classList.remove('footerActive');
 }
 
+function addHoveredState(){
+  menuButton.classList.add('hovered');
+  footer.classList.add('footerActive');
+}
+
+if (window.innerWidth < 768){
+  footer.removeEventListener('mouseover', addHoveredState);
+  footer.removeEventListener('mouseout', removeHoveredState);
+
+} else {
+  footer.addEventListener('mouseover', addHoveredState);
+  footer.addEventListener('mouseout', removeHoveredState);
+}
+
+
 menuButton.addEventListener('touchend', function () {
-    footer.classList.toggle('footerActive');
-    menuButton.classList.toggle('hovered');
+  if (menuButton.classList.contains('hovered')) {
+    footer.classList.remove('footerActive');
+    menuButton.classList.remove('hovered');
+  } else {
+    footer.classList.add('footerActive');
+    menuButton.classList.add('hovered');
+  }
 })
